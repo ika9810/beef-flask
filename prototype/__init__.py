7c4320794d3bed5cf0755bc219aecbc71e5c1c8d
JONGHEON LEE <ika7204@naver.com>
2022-01-18 02:21:58 +0900

    Add uri for test

--- prototype/__init__.py
+++ prototype/__init__.py
@@ -15,7 +15,7 @@ def createMetadata(grade, img):
         "metadata": {
             "description": "Beef NFT는 AI 소고기 등급 판별기를 통해 발행된 NFT로 AI 기반으로 소고기 이미지를 통해 소고기의 등급을 판별한 후 판별된 등급을 NFT에 저장해 인증서를 발급한다.",
             "external_url": "https://beef.honeyvuitton.com/", 
-            "image": img, 
+            "image": "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/5e36c107-6110-4245-b68c-41d7cb8c9c48/Untitled.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220117%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220117T171858Z&X-Amz-Expires=86400&X-Amz-Signature=c264ae63cb8c68bae8ba591dc9b0809d35b0ea99b4fe5939b31b1c182d4fa270&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22Untitled.png%22&x-id=GetObject", 
             "name": "Beef NFT",
             "attributes": [ {"grdade" : grade}, {"createdTime": str(datetime.datetime.now())},], 
         },
@@ -34,7 +34,7 @@ def createMetadata(grade, img):
     # "uri": "https://metadata-store.klaytnapi.com/9c7de118-cf48-8ab5-f186-4ed29b9cf6b7/13126799-5d29-06f8-ea1d-ff5f5165ce35.json"
     # }     //KAS Medata API 메타데이터 생성 부분 리턴 형태
 
-    return response.json()
+    return response.json()["uri"]
 
 def mintNFT(uri, tokenID):
     url = "https://kip17-api.klaytnapi.com/v1/contract/0xe3a390fdb12dafe2eb37d7829d11a18f37e59424/token"
